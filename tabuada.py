a97eeebf0ba488948245955b96da3dcff6f7bf93
guilhermerek <50538146+guilhermerek@users.noreply.github.com>
2019-12-14 21:56:26 -0300

    Update and rename ex 5 lista 2.py to tabuada.py

--- tabuada.py
+++ tabuada.py
@@ -1,6 +1,4 @@
 print('-'*15)
-print('Excercício 5')
-print('-'*15)
 primeira = int(input('Digite a primeira tabuada a aparecer:\n'))
 ultima = int(input('Digite a última tabuada à aparecer:\n'))
 for a in range(primeira, ultima +1):
